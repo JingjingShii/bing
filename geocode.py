8b6d9b8f54396d29b47bac24e3bcff2cd0bf9d37
jingjinggit config --global user.email 745287546@qq.com <745287546@qq.com>
2021-04-30 13:13:46 +1000

    Add short version of argument

--- geocode.py
+++ geocode.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
                         help='Show the location in the Google Map.')
     parser.add_argument('--to', '-t', type=str,
                         help='Output csv file path. ')
-    parser.add_argument('--verbose', '-ver', type=bool,
+    parser.add_argument('--verbose', '-b', type=bool,
                         help='Print out the result.')
     args = parser.parse_args()
 
